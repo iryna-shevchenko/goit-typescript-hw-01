4aa8ea2485dcdec610bba8fafc7639adc9978f0c
iryna-shevchenko <iryna.shevchenko.fullstack@gmail.com>
2024-09-17 20:35:46 +0300

    Finally work

---
+++ src/generics/4.ts
@@ -0,0 +1,21 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+const defaultUser: User = {
+  name: "",
+  surname: "",
+  email: "",
+  password: "",
+};
+function createOrUpdateUser(initialValues: Partial<User>) {
+  return { ...defaultUser, ...initialValues };
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
